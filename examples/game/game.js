5a259ebe3d4e9a1528267e305b60c95d05508628
Rizalxtc <115970773+Rizalxtc@users.noreply.github.com>
2022-12-30 12:19:23 +0700

    Update game.js

--- examples/game/game.js
+++ examples/game/game.js
@@ -3,7 +3,7 @@
  */
 /* eslint-disable no-console */
 
-const TOKEN = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
+const TOKEN = process.env.TELEGRAM_TOKEN || '5813387900:AAGzGpJHe7ADBkl6B_Lvm256-4SY5i0k1vc';
 const gameName = process.env.TELEGRAM_GAMENAME || 'YOUR_TELEGRAM_GAMENAME';
 // Specify '0' to use ngrok i.e. localhost tunneling
 let url = process.env.URL || 'https://<PUBLIC-URL>';
